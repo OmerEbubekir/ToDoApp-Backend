9cb09e94e7dd1c816fa8b8b3385d70bf95280941
HaciAbi <bekiryenicag06@gmail.com>
2026-05-16 16:09:21 +0300

    feat(todo): add protected todo controller with user isolation based on jwt claims

---
+++ src/Services/ToDoService/ToDoService.API/Controllers/TodoController.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ToDoService.Application.DTOs;
+using ToDoService.Application.Interfaces;
+
+namespace ToDoService.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly IToDoService _toDoService;
+
+        public TodoController(IToDoService toDoService)
+        {
+            _toDoService = toDoService;
+        }
+
+        private string GetUserId() => User.FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty;
+
+        [HttpGet]
+        public async Task<IActionResult> GetMyToDos()
+        {
+            var userId = GetUserId();
+            var items = await _toDoService.GetAllByUserIdAsync(userId);
+            return Ok(items);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatateToDo([FromBody] CreateToDoDto createToDoDto)
+        {
+            var userId = GetUserId();
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+
+            var item = await _toDoService.CreateAsync(createToDoDto, userId);
+            return StatusCode(201, item);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDo(Guid id)
+        {
+            var userId = GetUserId();
+
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+            var result = await _toDoService.DeleteAsync(id, userId);
+
+            if (!result)
+                return NotFound(new { message = "Görev bulunamadı.." });
+
+            return Ok(new { message = "Görev başarıyla silindi" });
+
+
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateToDo(Guid id, [FromBody] UpdateToDoDto updateToDoDto)
+        {
+            var userId = GetUserId();
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+            var item = await _toDoService.UpdateAsync(id, updateToDoDto, userId);
+            if (item == null)
+                return NotFound(new { message = "Görev bulunamadı.." });
+            return Ok(item);
+
+        }
+    }
+}
